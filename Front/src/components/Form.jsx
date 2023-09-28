1414ce83eea6766a93d132548593d5b239b64a3b
Sebastián Herrera <herrera5415@gmail.com>
2023-09-27 22:24:48 -0300

    Background del login
    Se agrega un fondo y un card al login

--- Front/src/components/Form.jsx
+++ Front/src/components/Form.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useSignIn, useIsAuthenticated } from "react-auth-kit";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import "./form.css";
 
 export default function Form() {
   const navigate = useNavigate();
@@ -24,23 +25,22 @@ export default function Form() {
     // Resetea el estado de error cuando el usuario cambia la contraseña
     setPasswordError("");
   }
-   //nombre:ID CONTRASEÑA:
+  //nombre:ID CONTRASEÑA:
   async function handleSubmit(e) {
     e.preventDefault();
-    try{
+    try {
       const response = await axios.post(
         "http://localhost:3000/login",
         formValues
       );
-        sigIn({
-          token: response.data.token,
-          expiresIn: 3600,
-          tokenType: "Bearer",
-          authState: { nombre: formValues.nombre },
-        });
-    }
-    catch(err){
-      setPasswordError(err.response.data.message)
+      sigIn({
+        token: response.data.token,
+        expiresIn: 3600,
+        tokenType: "Bearer",
+        authState: { nombre: formValues.nombre },
+      });
+    } catch (err) {
+      setPasswordError(err.response.data.message);
     }
   }
 
@@ -54,63 +54,81 @@ export default function Form() {
 
   return (
     <>
-      <div className="container">
-        <div className="row justify-content-center align-items-center vh-100">
-          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center">
-            <img
-              src="./public/Logo/Logo TechStream.png"
-              className="img-fluid"
-              alt="Logo TechStream"
-            />
-            <h2 className="pt-5">Inicio de sesión</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-4 pt-4">
-                <select
-                  name="nombre"
-                  onChange={handleChangeInput}
-                  className="form-select form-select-sm"
-                  aria-label="Small select example"
-                >
-                  <option selected>Seleccione un tipo de usuario</option>
-                  <option value="Administrador">Administrador</option>
-                  <option value="Logística">Logística</option>
-                  <option value="Taller">Taller</option>
-                </select>
-              </div>
-              <div className={`form-group ${(passwordError==="Contraseña incorrecta") ? "has-error" : ""}`}>
-                <input
-                  type="password"
-                  name="pass"
-                  className={`form-control ${
-                    (passwordError==="Contraseña incorrecta") ? "is-invalid" : ""
-                  }`}
-                  id=""
-                  placeholder="Ingrese contraseña"
-                  onChange={handleChangeInput}
-                />
-                {(passwordError==="Contraseña incorrecta")? (
-                  <div className="invalid-feedback text-danger">
-                    Contraseña incorrecta
-                  </div>
-                ):<></>}
-                <div className="d-grid gap-2 pt-4">
-                  <motion.div
-                    className="box"
-                    whileHover={{ scale: [null, 1.1, 1.1] }}
-                    transition={{ duration: 0.3 }}
-                  >
-                    <button className="btn btn-primary w-100" type="submit">
-                      Ingresar
-                    </button>
-                  </motion.div>
+
+      <div className="row justify-content-center align-items-center vh-100">
+        <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 text-center">
+          <div className="containergeneraladd">
+            <div className="container">
+              <div className="card">
+                <div className="card-content"></div>
+                        <img
+                          src="./public/Logo/Logo TechStream.png"
+                          className="img-fluid"
+                          alt="Logo TechStream"
+                        />
+                  <h2 className="pt-5">Inicio de sesión</h2>
+                  <form onSubmit={handleSubmit}>
+                    <div className="mb-4 pt-4">
+                      <select
+                        name="nombre"
+                        onChange={handleChangeInput}
+                        className="form-select form-select-sm"
+                        aria-label="Small select example"
+                      >
+                        <option selected>Seleccione un tipo de usuario</option>
+                        <option value="Administrador">Administrador</option>
+                        <option value="Logística">Logística</option>
+                        <option value="Taller">Taller</option>
+                      </select>
+                    </div>
+                    <div
+                      className={`form-group ${
+                        passwordError === "Contraseña incorrecta"
+                          ? "has-error"
+                          : ""
+                      }`}
+                    >
+                      <input
+                        type="password"
+                        name="pass"
+                        className={`form-control ${
+                          passwordError === "Contraseña incorrecta"
+                            ? "is-invalid"
+                            : ""
+                        }`}
+                        id=""
+                        placeholder="Ingrese contraseña"
+                        onChange={handleChangeInput}
+                      />
+                      {passwordError === "Contraseña incorrecta" ? (
+                        <div className="invalid-feedback text-danger">
+                          Contraseña incorrecta
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                      <div className="d-grid gap-2 pt-4">
+                        <motion.div
+                          className="box"
+                          whileHover={{ scale: [null, 1.1, 1.1] }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          <button
+                            className="btn btn-primary w-100"
+                            type="submit"
+                          >
+                            Ingresar
+                          </button>
+                        </motion.div>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
-            </form>
-          </div>
+            </div>
+          </div>       
         </div>
-      </div>
-    </>
-    
-  );
   
+    </>
+  );
 }
--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import NavbarInicio from '../components/NavbarInicio'
-import Form from '../components/Form'
+import NavbarInicio from "../components/NavbarInicio";
+import Form from "../components/Form";
+import ParticlesBackground from "../components/Particle";
+import Particle from "../components/Particle";
 export default function Home() {
   return (
     <>
+      
+        <Particle />
         <NavbarInicio></NavbarInicio>
         <Form></Form>
+     
     </>
-  )
+  );
 }
